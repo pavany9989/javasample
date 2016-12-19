df714a14db234f70602655957b9762785ad915d9
PAVAN CHOWDARY YALAMANCHILI <PAVAN CHOWDARY YALAMANCHILI>
2016-12-18 23:26:20 -0500

    changes in mysql password

--- src/com/demo/LoginServlet.java
+++ src/com/demo/LoginServlet.java
@@ -30,7 +30,7 @@ public class LoginServlet extends HttpServlet {
     	
     		try{
     			Class.forName("com.mysql.jdbc.Driver");
-    			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/upssample","root","");
+    			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/upssample","root","pavan");
     		} catch(Exception e) {
     			e.printStackTrace();
     		}
